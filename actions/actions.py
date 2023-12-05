99b5d4d991ffacf1a06535ec723127be65727f31
elisavetg <125077203+elisavetg@users.noreply.github.com>
2023-12-05 17:53:55 +0200

    Update actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,4 +1,5 @@
 from typing import Any, Text, Dict, List
+import pandas as pd
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -11,24 +12,22 @@ class GenreEntity(Action):
     def run(self, dispatcher: CollectingDispatcher,
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-         
-        genre_entity = next(tracker.get_latest_entity_values("genre"), None)
+        
+        csv_file_path = "/Users/Elisabeth/Downloads/archive/IMDB-Movie-Data.csv"
+        df = pd.read_csv(csv_file_path, header=0)
+ 
+        genre_entity_original = next(tracker.get_latest_entity_values("genre"), None)
+        genre_entity = genre_entity_original.lower()
+        
+        df["Genre"] = df["Genre"].apply(lambda x: x.lower())
 
-        if genre_entity:
-            dispatcher.utter_message(template=f"utter_genre_chosen", genre=genre_entity)
+        #filtered_df = df[df["Genre"] == genre_entity]
+        filtered_df = df[df["Genre"].apply(lambda genres: genre_entity in genres)]
+
+        if not filtered_df.empty:
+            recommended_movie = filtered_df.sample()["Title"].values[0]
+            dispatcher.utter_message(template="utter_movie_recommendation", movie=recommended_movie, genre=genre_entity)
         else:
             dispatcher.utter_message(template="utter_unknown_genre")
-
-        return[]
-
-class MovieChoice(Action):
-
-    def name(self) -> Text:
-         return "action_movie_choice"
-         
-    def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_ask_genre")
 
         return[]
